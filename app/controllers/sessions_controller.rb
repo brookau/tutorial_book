f2a08f4e9b09087f304ab2e347b75ed6f2f30752
Brook HES <settakorn@hiddenedge-studio.com>
2015-05-14 17:48:03 +0700

    Chapter8: Finish login/logout

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,33 @@
+class SessionsController < ApplicationController
+  def new
+  
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+      redirect_to user
+    else
+      flash.now[:danger] = 'Invalid email/password combination'
+      render 'new'
+    end
+  end
+
+  	# user = User.find_by(email: params[:session][:email].downcase)
+   #  if user && user.authenticate(params[:session][:password])
+   #    log_in user
+   #    params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+   #    redirect_to user
+   #  else
+   #    # Create an error message.
+   #    flash.now[:danger] = 'Invalid email/password combination'
+   #    render 'new'
+   #  end  
+
+  def destroy
+    log_out if logged_in?
+    redirect_to root_url
+  end
+end
